bc592a054f06b63bb45af16349ed15618a49f1af
Ender Bakır <enderyx_123@hotmail.com>
2024-03-19 17:07:49 +0300

    Added Feature.cs

---
+++ SignalRApi/DAL/Entities/Feature.cs
@@ -0,0 +1,19 @@
+﻿namespace SignalRApi.DAL.Entities
+{
+    public class Feature
+    {
+        public int FeatureId { get; set; }
+
+        public string Title1 { get; set; }
+
+        public string Description1 { get; set; }
+
+        public string Title2 { get; set; }
+
+        public string Description2 { get; set; }
+
+        public string Title3 { get; set; }
+
+        public string Description3 { get; set; }
+    }
+}
