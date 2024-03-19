ae090a864bb60d473823e8ee3ff4f21a8371851a
Ender Bakır <enderyx_123@hotmail.com>
2024-03-19 17:06:04 +0300

    Added About.cs

---
+++ SignalRApi/DAL/Entities/About.cs
@@ -0,0 +1,13 @@
+﻿namespace SignalRApi.DAL.Entities
+{
+    public class About
+    {
+        public int AboutId { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+    }
+}
