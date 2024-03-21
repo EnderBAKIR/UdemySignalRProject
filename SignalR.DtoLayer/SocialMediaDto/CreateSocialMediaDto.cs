6b316f40460e8d3b7ae5fda8b756a98c9767febe
Ender Bakır <enderyx_123@hotmail.com>
2024-03-21 12:34:37 +0300

    Added SocialMediaDto

---
+++ SignalR.DtoLayer/SocialMediaDto/CreateSocialMediaDto.cs
@@ -0,0 +1,11 @@
+﻿namespace SignalR.DtoLayer.SocialMediaDto
+{
+    public class CreateSocialMediaDto
+    {
+        public string Title { get; set; }
+
+        public string Url { get; set; }
+
+        public string Icon { get; set; }
+    }
+}
---
+++ SignalR.DtoLayer/SocialMediaDto/UpdateSocialMediaDto.cs
@@ -0,0 +1,13 @@
+﻿namespace SignalR.DtoLayer.SocialMediaDto
+{
+    public class UpdateSocialMediaDto
+    {
+        public int SocialMediaId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Url { get; set; }
+
+        public string Icon { get; set; }
+    }
+}
