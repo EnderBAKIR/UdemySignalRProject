e3c9eecfa78f605b3c9e4bd92192df8ce969d409
Ender Bakır <enderyx_123@hotmail.com>
2024-03-21 12:23:39 +0300

    Added ContactDto

---
+++ SignalR.DtoLayer/ContactDto/GetContactDto.cs
@@ -0,0 +1,15 @@
+﻿namespace SignalR.DtoLayer.ContactDto
+{
+    public class GetContactDto
+    {
+        public int ContactId { get; set; }
+
+        public string Location { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Mail { get; set; }
+
+        public string FooterDescription { get; set; }
+    }
+}
---
+++ SignalR.DtoLayer/ContactDto/UpdateContactDto.cs
@@ -0,0 +1,15 @@
+﻿namespace SignalR.DtoLayer.ContactDto
+{
+    public class UpdateContactDto
+    {
+        public int ContactId { get; set; }
+
+        public string Location { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Mail { get; set; }
+
+        public string FooterDescription { get; set; }
+    }
+}
