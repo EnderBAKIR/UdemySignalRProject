7be48ce6de2a188df38522bb608624ac72f0699d
Ender Bakır <enderyx_123@hotmail.com>
2024-03-21 12:36:43 +0300

    Added TestimonialDto

---
+++ SignalR.DtoLayer/TestimonialDto/CreateTestimonialDto.cs
@@ -0,0 +1,15 @@
+﻿namespace SignalR.DtoLayer.TestimonialDto
+{
+    public class CreateTestimonialDto
+    {
+        public string Name { get; set; }
+
+        public string Title { get; set; }
+
+        public string Comment { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public bool Status { get; set; }
+    }
+}
