0cfd9566aee0db275afd1a7038e46d4f77af7b51
Ender Bakır <enderyx_123@hotmail.com>
2024-03-21 12:33:06 +0300

    Added ProductDto

---
+++ SignalR.DtoLayer/ProductDto/UpdateProductDto.cs
@@ -0,0 +1,18 @@
+﻿namespace SignalR.DtoLayer.ProductDto
+{
+    public class UpdateProductDto
+    {
+        public int ProductId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public bool ProductStatus { get; set; }
+    }
+
+}
