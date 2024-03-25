9a0d43277846d3f78d3d869a6ee08c8cfe60a2af
Ender Bakır <enderyx_123@hotmail.com>
2024-03-25 16:57:31 +0300

    Added TestimonialMapping

--- SignalRApi/Mapping/AboutMapping.cs
+++ SignalRApi/Mapping/AboutMapping.cs
@@ -1,6 +1,5 @@
 ﻿using AutoMapper;
 using SignalR.DtoLayer.AboutDto;
-using SignalR.DtoLayer.SocialMediaDto;
 using SignalR.EntityLayer.Entities;
 
 namespace SignalRApi.Mapping
@@ -13,16 +12,6 @@ namespace SignalRApi.Mapping
           CreateMap<About , CreateAboutDto>().ReverseMap();
           CreateMap<About , UpdateAboutDto>().ReverseMap();
           CreateMap<About , GetAboutDto>().ReverseMap();
-        }
-    }
-    public class SocialMediaMapping : Profile
-    {
-        public SocialMediaMapping()
-        {
-            CreateMap<SocialMedia, ResultSocialMediaDto>().ReverseMap();
-            CreateMap<SocialMedia, CreateSocialMediaDto>().ReverseMap();
-            CreateMap<SocialMedia, UpdateSocialMediaDto>().ReverseMap();
-            CreateMap<SocialMedia, GetSocialMediaDto>().ReverseMap();
         }
     }
 }
