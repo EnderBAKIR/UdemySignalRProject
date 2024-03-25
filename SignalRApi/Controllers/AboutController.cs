4a2edb461ef89f4b88f3e946444f0bbb156bb61b
Ender Bakır <enderyx_123@hotmail.com>
2024-03-25 17:40:54 +0300

    Added AboutController =>API

---
+++ SignalRApi/Controllers/AboutController.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.AboutDto;
+using SignalR.EntityLayer.Entities;
+using SignalRApi.Mapping;
+
+namespace SignalRApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AboutController : ControllerBase
+    {
+        private readonly IAboutService _aboutService;
+        private readonly IMapper _mapper;
+
+        public AboutController(IAboutService aboutService, IMapper mapper)
+        {
+            _aboutService = aboutService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult AboutList()
+        {
+            var values = _aboutService.TGetListAll();
+            return Ok(values);
+        }
+        [HttpPost]
+        public IActionResult CreateAbout(CreateAboutDto createAboutDto)
+        {
+            var createAboutMap = _mapper.Map<About>(createAboutDto);
+            _aboutService.TAdd(createAboutMap);
+            return Ok("Hakkında Kısmı Başarılı Bir Şekilde Eklendi");
+        }
+        [HttpDelete]
+        public IActionResult DeleteAbout(int id)
+        {
+            var value = _aboutService.TGetById(id);
+            _aboutService.TDelete(value);
+            return Ok("Hakkımda Alanı Başarılı Bir Şekilde Silindi");
+        }
+        [HttpPut]
+        public IActionResult UpdateAbout(UpdateAboutDto updateAboutDto)
+        {
+            var updateAboutMap = _mapper.Map<About>(updateAboutDto);
+            _aboutService.TUpdate(updateAboutMap);
+            return Ok("Hakkımda Alanı Başarılı Bir Şekilde Güncellendi");
+        }
+        [HttpGet("GetAbout")]
+        public IActionResult GetAbout(int id)
+        {
+            var value = _aboutService.TGetById(id);
+            return Ok(value);
+        }
+
+    }
+}
--- SignalRApi/Program.cs
+++ SignalRApi/Program.cs
@@ -1,12 +1,15 @@
 using Microsoft.EntityFrameworkCore;
+using SignalR.BusinessLayer.Abstract;
+using SignalR.BusinessLayer.Concrete;
+using SignalR.DataAccessLayer.Abstract;
 using SignalR.DataAccessLayer.Concrete;
+using SignalR.DataAccessLayer.EntityFramework;
 using System;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -19,6 +22,10 @@ builder.Services.AddDbContext<SignalRDbContext>(cfg =>
     });
    
 });
+builder.Services.AddScoped<IAboutService, AboutManager>();
+builder.Services.AddScoped<IAboutDal, EfAboutDal>();
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
