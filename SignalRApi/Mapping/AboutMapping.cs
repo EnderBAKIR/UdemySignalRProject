a003166de15c03765c1c9a8e748c663c29fda359
Ender Bakır <enderyx_123@hotmail.com>
2024-03-25 16:56:33 +0300

    Added SocialMediaMapping

--- SignalRApi/Mapping/AboutMapping.cs
+++ SignalRApi/Mapping/AboutMapping.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using SignalR.DtoLayer.AboutDto;
+using SignalR.DtoLayer.SocialMediaDto;
 using SignalR.EntityLayer.Entities;
 
 namespace SignalRApi.Mapping
@@ -12,6 +13,16 @@ namespace SignalRApi.Mapping
           CreateMap<About , CreateAboutDto>().ReverseMap();
           CreateMap<About , UpdateAboutDto>().ReverseMap();
           CreateMap<About , GetAboutDto>().ReverseMap();
+        }
+    }
+    public class SocialMediaMapping : Profile
+    {
+        public SocialMediaMapping()
+        {
+            CreateMap<SocialMedia, ResultSocialMediaDto>().ReverseMap();
+            CreateMap<SocialMedia, CreateSocialMediaDto>().ReverseMap();
+            CreateMap<SocialMedia, UpdateSocialMediaDto>().ReverseMap();
+            CreateMap<SocialMedia, GetSocialMediaDto>().ReverseMap();
         }
     }
 }
