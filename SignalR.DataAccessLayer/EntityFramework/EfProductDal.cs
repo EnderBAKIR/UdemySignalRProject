607b7d3ddcc172b2ddfcfdb1a3bc0c73f818f5c4
Ender Bakır <enderyx_123@hotmail.com>
2024-03-26 14:35:42 +0300

    Added ResultProductWithCategoryDto

--- SignalR.DataAccessLayer/EntityFramework/EfProductDal.cs
+++ SignalR.DataAccessLayer/EntityFramework/EfProductDal.cs
@@ -1,4 +1,5 @@
-﻿using SignalR.DataAccessLayer.Abstract;
+﻿using Microsoft.EntityFrameworkCore;
+using SignalR.DataAccessLayer.Abstract;
 using SignalR.DataAccessLayer.Concrete;
 using SignalR.DataAccessLayer.Repositories;
 using SignalR.EntityLayer.Entities;
@@ -7,8 +8,18 @@ namespace SignalR.DataAccessLayer.EntityFramework
 {
     public class EfProductDal : GenericRepository<Product>, IProductDal
     {
+        private readonly SignalRDbContext _context;
+
         public EfProductDal(SignalRDbContext context) : base(context)
         {
+            _context = context;
+        }
+
+        public List<Product> GetProductsWithCategories()
+        {
+           var values = _context.Products.Include(x=>x.Category).ToList();
+
+            return values;
         }
     }
 }
